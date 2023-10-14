53aa350bf91fd59462f77d6e095710e143c76222
Rohit Masale <masalerohit246@gmail.com>
2023-10-14 21:33:52 +0530

    imgaes added and some changes in html,css and js file

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-var typed = new Typed('#element', { strings: ['<<i> Web Developer</i>./>'], typeSpeed: 60, loop: true });
+var typed = new Typed('#element', { strings: ['<<i> Web Developer</i>/>'], typeSpeed: 60, loop: true });
+
+// Function to handle smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust the offset as needed
+                behavior: 'smooth',
+            });
+        }
+    });
+});
